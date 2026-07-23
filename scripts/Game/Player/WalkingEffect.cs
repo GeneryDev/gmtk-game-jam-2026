ff308ec1428234ca3e2b4c04425324b165a048fd
FriendlyGuardian <tobin.rumsey@gmail.com>
2026-07-23 16:51:59 -0600

    fixed and switched

--- scripts/Game/Player/WalkingEffect.cs
+++ scripts/Game/Player/WalkingEffect.cs
@@ -1,13 +1,13 @@
+using Game.Entities;
 using Godot;
 using System;
 
 public partial class WalkingEffect : Node
 
 {
-	[Export] CpuParticles2D leftFootParticles;
-	[Export] CpuParticles2D rightFootParticles;
-	[Export] public double VelocityToParticles = 0.05;
-	[Export] public CharacterBody2D body;
+	[Export] GpuParticles2D Particles;
+	[Export] public float VelocityToParticles = 0.066f;
+	[Export] public MotionComponent MotionBody;
 	CpuParticles2D[] particles;
 	public override void _Ready()
 	{
@@ -16,26 +16,9 @@ public partial class WalkingEffect : Node
 
 	public override void _Process(double delta)
 	{
-		double velocityMagnitude = body.Velocity.Length();
-		GD.Print(velocityMagnitude);
+		double velocityMagnitude = MotionBody.LastObservedVelocity.Length();
+		Particles.AmountRatio = (float)velocityMagnitude * VelocityToParticles;
 		
-		//if (velocityMagnitude < 0.005f)
-		//{
-			//leftFootParticles.Emitting = false;
-			//rightFootParticles.Emitting = false;
-		//}
-		//else
-		//{
-			//leftFootParticles.Emitting = true;
-			//rightFootParticles.Emitting = true; 
-		//}
-
-		//int amount = (int) Math.Ceiling(velocityMagnitude * VelocityToParticles);
-		//// GD.Print(amount);
-		//leftFootParticles.Amount = amount;
-		//rightFootParticles.Amount = amount;
-		// Velocity magnitude reporting 0!??!? for all of its movement. That could not be right, and it think it is reading within the scope of its scene.
-		// DO NOT USE Dynamic steps. 
 	}
 	
 }
