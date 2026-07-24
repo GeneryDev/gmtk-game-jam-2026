32dfc9d59c26254396e16db285af31a1fd5d4be9
GeneryDev <generydev@gmail.com>
2026-07-24 16:25:23 -0400

    Add sprite y-sorting fix

---
+++ scripts/Game/Util/YSorted.cs
@@ -0,0 +1,51 @@
+﻿using Godot;
+
+namespace Game.Util;
+
+[GlobalClass]
+public partial class YSorted : Node
+{
+    private Node2D _parent;
+    
+    public float SortKey { get; private set; }
+
+    public void UpdateSortKey()
+    {
+        SortKey = _parent.GlobalPosition.Y;
+    }
+
+    public void SetZIndex(int zIndex)
+    {
+        _parent.ZIndex = zIndex;
+    }
+
+    public override void _EnterTree()
+    {
+        base._EnterTree();
+        YSortSystem.Instance?.Add(this);
+    }
+
+    public override void _ExitTree()
+    {
+        base._ExitTree();
+        YSortSystem.Instance?.Remove(this);
+    }
+
+    public override void _Notification(int what)
+    {
+        base._Notification(what);
+        switch ((long)what)
+        {
+            case NotificationParented:
+            {
+                _parent = GetParentOrNull<Node2D>();
+                break;
+            }
+            case NotificationUnparented:
+            {
+                _parent = null;
+                break;
+            }
+        }
+    }
+}
