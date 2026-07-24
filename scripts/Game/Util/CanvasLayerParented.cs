0149f5f87fe072ec58e62cf9dab0e64b31fd509d
GeneryDev <generydev@gmail.com>
2026-07-24 16:06:03 -0400

    Group background, foreground and overlays into canvas layers

---
+++ scripts/Game/Util/CanvasLayerParented.cs
@@ -0,0 +1,33 @@
+﻿using Godot;
+
+namespace Game.Util;
+
+[GlobalClass]
+public partial class CanvasLayerParented : CanvasLayer
+{
+    private CanvasItem _parent;
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+        this.Transform = _parent.GetGlobalTransform();
+    }
+
+    public override void _Notification(int what)
+    {
+        base._Notification(what);
+        switch ((long)what)
+        {
+            case NotificationParented:
+            {
+                _parent = GetParentOrNull<CanvasItem>();
+                break;
+            }
+            case NotificationUnparented:
+            {
+                _parent = null;
+                break;
+            }
+        }
+    }
+}
