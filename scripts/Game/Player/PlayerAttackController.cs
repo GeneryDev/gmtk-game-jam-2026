55fe8a834735d1034460c373e39f9c9dc41380fc
FriendlyGuardian <tobin.rumsey@gmail.com>
2026-07-25 22:52:12 -0600

    Player attack temp SFX

--- scripts/Game/Player/PlayerAttackController.cs
+++ scripts/Game/Player/PlayerAttackController.cs
@@ -1,4 +1,5 @@
 ﻿using Game.Entities;
+using GDF.Audio;
 using GDF.Composition;
 using GDF.Data;
 using GDF.Input;
@@ -11,6 +12,7 @@ public partial class PlayerAttackController : Node
 {
     [Export] public GdfInputAction AttackAction;
     [Export] public float AttackInterval = 0.2f;
+    [Export] public GdfAudioPlayer AttackSFX;
     
     private ComponentCache<GdfPlayerInput> _playerInput;
     private ComponentCache<PlayerController> _playerController;
@@ -71,6 +73,8 @@ public partial class PlayerAttackController : Node
 
     public void Attack()
     {
+        AttackSFX.playing = true;
+
         var body = _motionComponent.Get(this).Body;
         if (AttackScene == null) return;
         var attackContext = new AttackInstanceContext(Owner, body.GlobalPosition, _targetPosGlobal);
