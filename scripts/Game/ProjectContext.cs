ff3a6a28d8d13ca362800b25228dc618f720fadf
GeneryDev <generydev@gmail.com>
2026-07-26 02:59:30 -0400

    Add version number to main menu

---
+++ scripts/Game/ProjectContext.cs
@@ -0,0 +1,23 @@
+﻿using GDF.Data;
+using GDF.Data.Static;
+using Godot;
+
+namespace Game;
+
+[StaticDataContext("project_context")]
+public struct ProjectContext : IDataContext
+{
+    public bool GetContextVariable(string key, string input, ref Variant output, IDataQueryOptions options)
+    {
+        switch (key)
+        {
+            case "version":
+            {
+                output = "v" + ProjectSettings.GetSetting("application/config/version");
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
